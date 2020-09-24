1fd7d85479fd806a5fa5ee8e8a88a3839f6a48bd
Damir Dautov <dds9727@gmail.com>
2020-09-24 12:20:29 +0500

    Updates

---
+++ precache-manifest.8c8fa66feadf67aed1d6a3243d70dbda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e6d9e80804dc2a0bb9bd494bdf6f40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef40ec6406e61ea3ea7e",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "38b3dde4ea2863a6f89b",
+    "url": "./static/css/main.c957e2d8.chunk.css"
+  },
+  {
+    "revision": "ef40ec6406e61ea3ea7e",
+    "url": "./static/js/2.847d612c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.847d612c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b3dde4ea2863a6f89b",
+    "url": "./static/js/main.89b7b93a.chunk.js"
+  },
+  {
+    "revision": "f8dbc2b3edf0fd274ca0",
+    "url": "./static/js/runtime-main.fc70c573.js"
+  }
+]);
